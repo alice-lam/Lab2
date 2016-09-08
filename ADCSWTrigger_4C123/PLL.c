c4329f0587e5c9c86617d002e68b1cc140174557
aznfrosting <alice94@sbcglobal.net>
2016-09-07 20:49:10 -0500

    Prelab 3

--- ADCSWTrigger_4C123/PLL.c
+++ ADCSWTrigger_4C123/PLL.c
@@ -24,6 +24,7 @@
 #include <stdint.h>
 #include "PLL.h"
 #include "../inc/tm4c123gh6pm.h"
+#include "Timer1.h"
 
 // The #define statement SYSDIV2 in PLL.h
 // initializes the PLL to the desired frequency.
@@ -70,6 +71,28 @@ void PLL_Init(uint32_t freq){
   while((SYSCTL_RIS_R&SYSCTL_RIS_PLLLRIS)==0){};
   // 6) enable use of PLL by clearing BYPASS
   SYSCTL_RCC2_R &= ~SYSCTL_RCC2_BYPASS2;
+}
+void (*PeriodicTask)(void);   // user function
+// ***************** TIMER1_Init ****************
+// Activate TIMER1 interrupts to run user task periodically
+// Inputs:  task is a pointer to a user function
+//          period in units (1/clockfreq)
+// Outputs: none
+void Timer1_Init(void(*task)(void), uint32_t period){
+  SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1
+  PeriodicTask = task;          // user function
+  TIMER1_CTL_R = 0x00000000;    // 1) disable TIMER1A during setup
+  TIMER1_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER1_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER1_TAILR_R = 0xFFFFFFFF;    // 4) reload value
+  TIMER1_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER1_ICR_R = 0x00000001;    // 6) clear TIMER1A timeout flag
+// TIMER1_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0xFFFF00FF)|0x00008000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 37, interrupt number 21
+//  NVIC_EN0_R = 1<<21;           // 9) enable IRQ 21 in NVIC
+//  TIMER1_CTL_R = 0x00000001;    // 10) enable TIMER1A
 }
 
 
